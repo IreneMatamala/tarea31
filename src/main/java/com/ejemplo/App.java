58184f6cb0d5245b5f8102e34e6f641cb9b8e9cf
Irene Matamala Muñoz <136511839+IreneMatamala@users.noreply.github.com>
2025-10-07 17:42:32 +0200

    Create App.java

---
+++ src/main/java/com/ejemplo/App.java
@@ -0,0 +1,11 @@
+package com.ejemplo;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("¡Hola Mundo desde Maven!");
+    }
+    
+    public static int sumar(int a, int b) {
+        return a + b;
+    }
+}
